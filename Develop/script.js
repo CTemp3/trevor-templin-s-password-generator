cc749cd658496c8a2642940b994039fb69006761
Trevor Templin <c.trevor.templin@gmail.com>
2021-11-14 22:10:49 -0700

    trying to add characters to the password, found one method via online research, but want to try to devise a different method to avoid outright copying someone else's idea

--- Develop/script.js
+++ Develop/script.js
@@ -2,25 +2,146 @@
 
 var generateBtn = document.querySelector("#generate");
 
-var charslconly = "abcdefghijklmnopqrstuvwxyz";
-var charslttrsonly = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var charslwc = "abcdefghijklmnopqrstuvwxyz";
+var charslttr = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var charslttrnmbr = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var charscmplt = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var charslwcnmbr = "0123456789abcdefghijklmnopqrstuvwxyz";
+var charslwcspcl = "abcdefghijklmnopqrstuvwxyz!@#$%^&*()";
+var charslwcspclnmbr = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()";
+var charslttrspcl = "abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-var inptpsswrdlngth = window.prompt("How long would you like your password to be? Please use numeric values only.")
-var psswrdlngth = parseInt(inptpsswrdlngth);
+var psswrdgen = function() {
 
-console.log(psswrdlngth);
+  var inptpsswrdlngth = window.prompt("How long would you like your password to be? Please use a numeric value between 8 and 128.")
 
-var cse = window.prompt("Would you like to include capitalized letters or only lower case? Please type yes or no.")
-var casecorrect = cse.toLowerCase();
+  while(true) {
 
-console.log(casecorrect);
+    var psswrdlngth = parseInt(inptpsswrdlngth);
+    console.log(psswrdlngth);
+  
+    if(psswrdlngth > 127 || psswrdlngth < 7) {
+    
+      var inptpsswrdlngth = window.prompt("The password length you input is invalid. Please try again!");
 
-var num = window.prompt("Would you like to include numbers? Please type yes or no.")
-var numcorrect = num.toLowerCase();
+      continue;
 
-console.log(numcorrect);
+    } else if(psswrdlngth <= 128 || psswrdlngth >= 8) {
+
+    break;  
+
+    }
+
+  }
+
+  var cse = window.confirm("Would you like to include capitalized letters? Select okay for capital & lower case, cancel for only lower case");
+      
+      console.log(cse);
+
+      if(cse = true) {
+
+        var num = window.confirm("Would you like to include numbers? Select okay to include, cancel to exclude");
+        console.log(num);
+
+        if(num = true) {
+
+          var spcl = window.confirm("Would you like to include special characters? Select okay to include, cancel to exclude");
+
+          console.log(spcl);
+
+          if(spcl = true) {
+
+            // use charscmplt here
+            for(var i = 1; i <= psswrdlngth; i++) {
+
+              var pickanumber = Math.random() * (charscmplt.length - 1) +1;
+              var roundednumber = Math.round(pickanumber);
+              console.log(pickanumber);
+              console.log(roundednumber);
+              console.log(charscmplt.length);
+
+
+              
+
+              
+
+            }
+
+          } else {
+
+            // use charslttrnmbr here
+
+          }
+
+        } else {
+
+          var spcl = window.confirm("Would you like to include special characters? Select okay to include, cancel to exclude");
+
+          console.log(spcl);
+
+          if(spcl = true) {
+
+            // use charslttrspcl here
+
+          } else {
+
+            // use charslttr here
+
+          }
+
+        }
+
+      } else {
+
+        var num = window.confirm("Would you like to include numbers? Select okay to include, cancel to exclude");
+        console.log(num);
+
+        if(num = true) {
+
+          var spcl = window.confirm("Would you like to include special characters? Select okay to include, cancel to exclude");
+
+          console.log(spcl);
+
+          if(spcl = true) {
+
+            // use charslwcspclnmbr here
+
+          } else {
+
+            // use charslwcnmbr here
+
+          }
+
+        } else {
+
+          var spcl = window.confirm("Would you like to include special characters? Select okay to include, cancel to exclude");
+
+          console.log(spcl);
+
+          if(spcl = true) {
+
+            // use charslwcspcl here
+
+          } else {
+
+            // use charslwc here
+
+          }
+
+        }
+
+        }
+
+}
+
+psswrdgen();
+
+
+
+
+
+
+
 
 // Write password to the #password input
 
